5f232891e8f3d352189f3af82519f962e329e00a
Arne Kiesewetter <banane9@live.de>
2023-03-08 18:06:33 +0100

    Add DynamicVariableSpace helpers

---
+++ NeosCCF/DynamicVariable.cs
@@ -0,0 +1,71 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeosCCF
+{
+    public abstract class DynamicVariable
+    {
+        private static readonly Type bareDynamicVariableType = typeof(DynamicValueVariable<>);
+        private static readonly Dictionary<Type, ConstructorInfo> genericDynamicVariableConstructors = new();
+        public string Name { get; }
+
+        public abstract Type Type { get; }
+
+        public object Value
+        {
+            get => getValue();
+            set => setValue(value);
+        }
+
+        internal DynamicVariable(string name)
+        {
+            Name = name;
+        }
+
+        public static DynamicVariable Create(DynamicVariableIdentity identity, DynamicVariableSpace.ValueManager manager)
+        {
+            if (!genericDynamicVariableConstructors.TryGetValue(identity.Type, out var constructor))
+            {
+                constructor = bareDynamicVariableType.MakeGenericType(identity.Type).GetConstructors()[0];
+                genericDynamicVariableConstructors.Add(identity.Type, constructor);
+            }
+
+            return (DynamicVariable)constructor.Invoke(new object[] { manager });
+        }
+
+        protected abstract object getValue();
+
+        protected abstract void setValue(object value);
+    }
+
+    public sealed class DynamicVariable<T> : DynamicVariable
+    {
+        private readonly DynamicVariableSpace.ValueManager<T> valueManager;
+
+        public int ReadableValues => valueManager.ReadableValueCount;
+        public override Type Type { get; } = typeof(T);
+
+        public new T Value
+        {
+            get => valueManager.Value;
+            set => valueManager.SetValue(value);
+        }
+
+        public int Values => valueManager.ValueCount;
+
+        public DynamicVariable(DynamicVariableSpace.ValueManager<T> manager) : base(manager.Name)
+        {
+            valueManager = manager;
+        }
+
+        protected override object getValue() => Value;
+
+        protected override void setValue(object value) => Value = (T)value;
+    }
+}
---
+++ NeosCCF/DynamicVariableSpaceHelper.cs
@@ -0,0 +1,48 @@
+﻿using FrooxEngine;
+using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeosCCF
+{
+    public static class DynamicVariableSpaceHelper
+    {
+        public static DynamicVariable<T> GetVariable<T>(this DynamicVariableSpace space, string name)
+        {
+            return space.GetVariables<T>().First(variable => variable.Name == name);
+        }
+
+        public static IEnumerable<DynamicVariable> GetVariables(this DynamicVariableSpace space, string name)
+        {
+            return space.GetVariables(variable => variable.Name == name);
+        }
+
+        public static IEnumerable<DynamicVariable<T>> GetVariables<T>(this DynamicVariableSpace space)
+        {
+            var type = typeof(T);
+
+            return space.GetVariables(variable => variable.Type == type).Cast<DynamicVariable<T>>();
+        }
+
+        public static IEnumerable<DynamicVariable> GetVariables(this DynamicVariableSpace space, Predicate<DynamicVariable> predicate)
+        {
+            foreach (var variable in space.GetVariables())
+                if (predicate(variable))
+                    yield return variable;
+        }
+
+        public static IEnumerable<DynamicVariable> GetVariables(this DynamicVariableSpace space)
+        {
+            var enumerator = ((IDictionary)Traverse.Create(space).Field("_dynamicValues").GetValue()).GetEnumerator();
+
+            while (enumerator.MoveNext())
+            {
+                yield return DynamicVariable.Create(new DynamicVariableIdentity(enumerator.Key), (DynamicVariableSpace.ValueManager)enumerator.Value);
+            }
+        }
+    }
+}
